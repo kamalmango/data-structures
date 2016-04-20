ca994e800d8e75037c48a716bbe8225c7f247b18
Kamal Mango <kamal.mango7@gmail.com>
2016-04-20 14:48:25 -0700

    Complete functional Stack and Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var length = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[length] = value;
+    length++;
   };
 
   someInstance.dequeue = function() {
+    
+    var dequeued = storage[0];
+    delete storage[0];
+    for (var i = 0; i < length; i++) {
+      storage[i] = storage[i+1];
+    }
+    length --;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return length >= 0 ? length : 0;
   };
 
   return someInstance;
