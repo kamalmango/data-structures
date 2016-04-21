8d5adf848c2233878496917e01a84d39a699c18f
kamalmango <kamal.mango7@gmail.com>
2016-04-20 18:25:16 -0700

    Coplete Stack and Queue for functional-shared pattern

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,29 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.length = 0;
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(string) {
+  this[this.length] = string;
+  this.length++;
+};
 
+queueMethods.dequeue = function() {
+  var dequeued = this[0];
+  delete this[0];
+  for (var i = 0; i < this.length; i++) {
+    this[i] = this[i+1];
+  }
+  this.length--;
+  return dequeued;
+};
+
+queueMethods.size =function() {
+  return this.length >= 0 ? this.length : 0;
+}
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,44 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.length = 0;
+  _.extend(someInstance, stackMethods);
+  return someInstance;
+
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this[this.length] = value;
+  this.length++
+};
+
+stackMethods.pop = function() {
+  var popped = this[this.length-1]
+  delete this[this.length-1];
+  this.length--;
+  return popped;
+};
+
+stackMethods.size = function() {
+  return this.length > 0 ? this.length : 0;
+};
+
+/*
+var stackMethods = {
+  push : function(value) {
+    this.length = value;
+    length ++;
+  },
+  pop : function() {
+    length --;
+  },
+  size : function() {
+    return length;
+  }
+};
+*/
 
 
