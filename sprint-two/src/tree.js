34e2d77657c382b9f234a88be0a925fa56a7137f
kamalmango <kamal.mango7@gmail.com>
2016-04-21 14:00:26 -0700

    Complete Tree with original specs

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,46 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target, base, result) {
+  /*
+  base = base || this.children;
+
+  for (var i = 0; i < base.length; i++) {
+    //(base[i].value === target) && (result = true);
+    if (base[i].value === target) {
+      return true;
+    } else if (base[i].children.length === 0) {
+      return false;
+    } else {
+      return this.contains(target, base[i].children);
+    }
+  }
+  */
+  
+  
+  result = result || false;
+  base = base || this.children;
+  for (var i = 0; i < base.length; i++) {
+    if (base[i].value === target) {
+      result = true
+    } else {
+      result = this.contains(target, base[i].children, result);
+    }
+  }
+  
+  return result;
+  
 };
 
 
