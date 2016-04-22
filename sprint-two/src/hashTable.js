7b86555b1daf4de62de108c33895fd4e163d1cdb
kamalmango <kamal.mango7@gmail.com>
2016-04-22 13:21:48 -0700

    Store values that were insterted on Hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,10 +7,16 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucketArray = LimitedArray(2);
+  this._storage.set(index, bucketArray);
+  var keyValue = this._storage.get(index);
+  keyValue.set(0, k);
+  keyValue.set(1, v);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index).get(1);
 };
 
 HashTable.prototype.remove = function(k) {
