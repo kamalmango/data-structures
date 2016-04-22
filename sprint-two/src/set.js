3be869d37722bf0328e3807738e37cd7f1a018f0
Kamal Mango <kamal.mango7@gmail.com>
2016-04-21 23:22:35 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,40 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  //set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
+  //this[item] = item;
 };
 
 setPrototype.contains = function(item) {
+    var result = false;
+    for (var key in this._storage) {
+      if (this._storage[key] === item) {
+        result = true;
+      }
+    }
+    return result;
+  };
+  /*
+  var result = false;
+  for (var key in this) {
+    if (this[key] === item) {
+      result = true;
+    }
+  }
+  return result;
 };
+*/
 
 setPrototype.remove = function(item) {
+  delete this._storage;
+  //delete this[item];
 };
 
 /*
