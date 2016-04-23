3c8d404dd4c2ab8c757faba8c9ce23f514049664
kamalmango <kamal.mango7@gmail.com>
2016-04-22 21:42:53 -0700

    add functions to binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,62 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(BinarySearchTreeMethods);
+  //newTree[value] = newTree;
+  newTree.value = value;
+  newTree.right = null;
+  newTree.left = null;
+  return newTree;
 };
 
+BinarySearchTreeMethods = {};
+
+BinarySearchTreeMethods.insert = function(value, base) {
+  
+  base = base || this.value;
+  if (value < base) {
+    if (this.left) {
+      this.insert(value, base.left);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  } else {
+    if (this.right) {
+      this.insert(value, base.right);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+  
+  /*
+  if (value < this.value) {
+    if (this.left) {
+      this.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  } else {
+    if (this.right) {
+      this.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+  */
+  /*
+  if (value < this.value) {
+    this.left = BinarySearchTree(value);
+  } else {
+    this.right = BinarySearchTree(value);
+  }
+  */
+};
+
+BinarySearchTreeMethods.contains = function() {
+
+};
+
+BinarySearchTreeMethods.depthFirstLog = function() {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
