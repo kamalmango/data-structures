47abbd60f0ced1c0dd2b4abefb3248b9a5e5cf6a
Kamal Mango <kamal.mango7@gmail.com>
2016-04-24 13:56:19 -0700

    fix linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,6 +4,71 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newTail = Node(value);
+
+    if (!list.head) {
+      list.head = newTail;
+    }
+
+    if (list.tail) {
+      list.tail.next = newTail;
+    }
+
+    list.tail = newTail;
+  };
+
+  list.removeHead = function() {
+    if (list.head === null) {
+      return null;
+    }
+    var currentHead = list.head;
+    list.head = list.head.next;
+    return currentHead.value;
+  };
+
+  list.contains = function(target) {
+    var node = list.head;
+
+     while (node) {
+       if (node.value === target) {
+         return true;
+       }
+
+       node = node.next;
+     }
+
+     return false;
+  };
+    /*
+    if (list.head.value === null) {
+      return false;
+    } else if (list.head.value === target) {
+      return false;
+    }else {
+      return list.contains(list.head.next);
+    }
+  };
+  */
+
+  return list;
+};
+
+var Node = function(value) {
+  var node = {};
+
+  node.value = value;
+  node.next = null;
+
+  return node;
+};
+
+/*
+var LinkedList = function() {
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+  list.addToTail = function(value) {
     list.tail = Node(value);
     (list.head === null) && (list.head = list.tail);
   };
@@ -35,6 +100,7 @@ var Node = function(value) {
 
   return node;
 };
+*/
 
 /*
  * Complexity: What is the time complexity of the above functions?
